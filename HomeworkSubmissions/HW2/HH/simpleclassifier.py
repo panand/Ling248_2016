7603f2e0b92260c403331cc2cf86217f3b40afd2
hhirayam <hhirayam@ucsc.edu>
2016-02-12 13:23:22 -0800

    Update simpleclassifier.py
    A simple classifier that only looks at the most frequent sense for each word

--- HomeworkSubmissions/HW2/HH/simpleclassifier.py
+++ HomeworkSubmissions/HW2/HH/simpleclassifier.py
@@ -3,28 +3,22 @@ from collections import Counter
 import collections
 from collections import defaultdict
 import os
-cnt=Counter()
-newdic={} #make dictionary
-lemmalist=[]
-docountacc=0
+
+docountacc=0 #setting basic counters
 docounterr=0
-docount=0
 makecountacc=0
 makecounterr=0
-makecount=0
 workcountacc=0
 workcounterr=0
-for filename in os.listdir(os.getcwd()):
+for filename in os.listdir(os.getcwd()): #applying processes to all files in the directory
     code=open(filename,'r')
     soup = BeautifulSoup(code, 'lxml')
     for words in soup.find_all('wf'):
         if words.get('lemma')=='do':
             if words.get('lexsn')=='2:41:01::':
                 docountacc+=1
-                print 'correct', filename
             else:
                 docounterr+=1
-                print 'error', filename
     for words in soup.find_all('wf'):
         if words.get('lemma')=='make':
             if words.get('lexsn')== '2:41:00::':
